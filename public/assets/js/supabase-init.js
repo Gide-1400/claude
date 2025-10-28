9c7360dba1f6129bae94915d5a7c286fee0c0a14
Gide <111868425+Gide-1400@users.noreply.github.com>
2025-10-28 18:31:58 +0300

    Delete public directory

--- public/assets/js/supabase-init.js
+++
@@ -1,5 +0,0 @@
-const supabaseUrl = 'https://chmistqmcmmmjqeanudu.supabase.co';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY'; // This should be your public anon key
-const supabase = supabase.createClient(supabaseUrl, supabaseAnonKey);
-
-window.supabase = supabase; // Make supabase globally available
